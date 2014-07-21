01413e369ff8b05e657f2a594e545008828b16b6
Marco Westerhof <m.westerhof@lukkien.com>
2014-07-21 10:08:25 +0200

    added some docs

--- README.md
+++ README.md
@@ -3,3 +3,18 @@ PYJeeves
 
 
 __work in progress__
+
+This project should eventually implement various database-backed tricks and shortcuts, to streamline human-computer interaction in a terminal environment.
+
+As it stands, it currently implements a high-level database wrapper, tailored to an SQLite backend.
+
+
+TODO
+----
+
+* Support automatic primary keys
+* Support relations
+* Support other SQL backends
+
+* Plugin system
+* Further tools
---
+++ example.py
@@ -0,0 +1,42 @@
+'''
+This module demonstrates the use of the DBModel base class. By simply creating
+your own subclass, you instruct the database layer to create an appropriate
+table for it. Any values defined in the class will correspond to a column, its
+type will be matched to the value type in the class, and the actual value will
+be used as a default, if we attempt to save the object without specifying that
+value.
+'''
+
+from jeeves import DBModel
+
+
+# the name of our class and (lowercased) table
+class Person(DBModel):
+    # the table should contain a column called "first_name"
+    # datatype CHAR, default value ""
+    first_name = ''
+
+    # the same for "last_name"
+    last_name = ''
+
+    # and also an INT called "age"
+    age = 0
+
+    # we can define methods and properties as we please
+    # this just makes "print <person object>" print a nice formatted string
+    def __repr__(self):
+        return '{0} {1} age {2}'.format(
+            self.first_name,
+            self.last_name,
+            self.age
+        )
+
+
+# and we can make as many classes as we like
+class Test(DBModel):
+    foo = 1
+    bar = 2
+    baz = ''
+
+    def __repr__(self):
+        return self.foo
